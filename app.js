f9bff1b845a4f16d23d9cedc5b2398a549e47bd0
Selt0 <chewedgum12@gmail.com>
2020-08-09 14:42:52 -0500

    Add campground form. Add grid style to campground page

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 5000;
 
+let campgrounds = [
+  { name: 'Salmon Creek', image: 'images/pexels-snapwire-699558.jpg' },
+  { name: 'Granite Hill', image: 'images/pexels-xue-guangjian-1687845.jpg' },
+  { name: 'Mountain Goat Rest', image: 'images/pexels-guduru-ajay-bhargav-939723.jpg' },
+  { name: 'Itasca', image: 'images/pexels-mac-destroir-2662816.jpg' },
+  { name: 'Salmon Creek', image: 'images/pexels-snapwire-699558.jpg' },
+  { name: 'Granite Hill', image: 'images/pexels-xue-guangjian-1687845.jpg' },
+  { name: 'Mountain Goat Rest', image: 'images/pexels-guduru-ajay-bhargav-939723.jpg' },
+  { name: 'Itasca', image: 'images/pexels-mac-destroir-2662816.jpg' },
+];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -11,14 +24,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-  let campgrounds = [
-    { name: 'Salmon Creek', image: 'images/pexels-snapwire-699558.jpg' },
-    { name: 'Granite Hill', image: 'images/pexels-xue-guangjian-1687845.jpg' },
-    { name: 'Mountain Goat Rest', image: 'images/pexels-guduru-ajay-bhargav-939723.jpg' },
-    { name: 'Itasca', image: 'images/pexels-mac-destroir-2662816.jpg' },
-  ];
-
   res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => {
+  // get data from form and add to campgrounds
+  let newCampground = {
+    name: req.body.name,
+    image: req.body.image,
+  };
+
+  campgrounds.push(newCampground);
+  // redirect back to campground
+  res.redirect('campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('newCamp.ejs');
 });
 
 app.listen(port, () => {
