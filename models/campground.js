9da8b7f8b6900437cd58eafd9e18c3aa081a701b
Selt0 <chewedgum12@gmail.com>
2020-08-16 12:44:17 -0500

    Refactor code, add models directory

---
+++ models/campground.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String,
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+});
+
+export default mongoose.model('Campground', campgroundSchema);
